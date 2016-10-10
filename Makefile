cfdc2866f85b5f4e52726d61877b2e1d8baf86ab
cassidylaidlaw <cassidy256@gmail.com>
2016-10-10 13:37:12 -0400

    Added download_github_repos.py script

--- Makefile
+++ Makefile
@@ -3,7 +3,17 @@ PYTHON=python3
 
 DATADIR=data
 
+.PHONY : download
+
 # Scrape awesome-rails GitHub to get a list of rails repositories
 $(DATADIR)/ruby-repos/awesome-rails-repos.csv : scripts/scrape_awesome_rails.py
 	mkdir -p $(dir $@)
 	$(PYTHON) $^ $@
+
+# Download GitHub repos (URLs are found in column 2 (0-indexed) of csv file)
+$(DATADIR)/ruby-repos/%.downloaded : scripts/download_github_repos.py $(DATADIR)/ruby-repos/%.csv
+	mkdir -p $(dir $@)
+	$(PYTHON) $^ 2 $(dir $@)
+	
+download : $(DATADIR)/ruby-repos/awesome-rails-repos.downloaded
+	
---
+++ scripts/download_github_repos.py
@@ -0,0 +1,34 @@
+import _path_config
+
+import sys
+import csv
+import os
+import subprocess
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: python3 download_github_repos.py repos.csv column outdir')
+        print('Downloads all github repositories found in the specified column (0-indexed)')
+        print('of repos.csv as URLs and places them in outdir.')
+    else:
+        _, repos_csv_fname, column_num, outdir = sys.argv
+        column_num = int(column_num)
+        
+        with open(repos_csv_fname, 'r') as repos_csv_file:
+            repos_csv = csv.reader(repos_csv_file)
+            next(repos_csv) # Skip header row
+        
+            # Change directories to output directory
+            old_cwd = os.getcwd()
+            os.chdir(outdir)
+        
+            for row in repos_csv:
+                repo_url = row[column_num]
+                subprocess.call(['git', 'clone', repo_url])
+                
+            # Change back
+            os.chdir(old_cwd)
+            
+            # And touch .downloaded file to help Makefile work
+            downloaded_fname = repos_csv_fname[:-4] + '.downloaded'
+            open(downloaded_fname, 'w').close()
